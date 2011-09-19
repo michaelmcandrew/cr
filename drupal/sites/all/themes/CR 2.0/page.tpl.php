edac5614ba1eee49f0b4284257d185a77e29f02b
KatyMoon <katyjockelson@thirdsectordesign.org>
2011-09-19 16:38:11 +0100

    adding footer region to page.tpl.php

--- drupal/sites/all/themes/CR 2.0/page.tpl.php
+++ drupal/sites/all/themes/CR 2.0/page.tpl.php
@@ -133,6 +133,7 @@
 				<div class="clear"></div>
 			</div>
 			<div id="footer">
+				<?php print $nav; ?>
 				<span><?php print $footer_message; ?></span>
 			</div>	
 		<?php print $closure; ?>
